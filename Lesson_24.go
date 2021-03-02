d7f6e4e2e4cbd1caedc7738e1d9a56181ab26723
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-03-03 00:03:32 +0900

    Lesson24

--- Lesson_24.go
+++ Lesson_24.go
@@ -5,13 +5,15 @@ import (
 )
 
 func main() {
-	age := 18
+	x := 10
+	y := 12
 
-	if age >= 20 {
+	if age := x + y; age >= 20 {
 		fmt.Println("adult")
 	} else if age == 0 {
 		fmt.Println("baby")
 	} else {
 		fmt.Println("child")
 	}
+	fmt.Println(age)
 }
